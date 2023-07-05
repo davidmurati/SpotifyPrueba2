f5ec707c02266a5a12190f4ffa16f738030bb7b2
David Murati <davidmurati1@gmail.com>
2023-07-05 08:09:58 -0400

    5 commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "./components/home/Home";
+import Login from "./Login";
+import Principal from "./components/principal/Principal";
+import Registro from "./components/Registro/Registro";
+import Modificar from "./components/modificar/modificar";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+     <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/Login" component={Login} />
+      <Route path="/Principal" component={Principal} />
+      <Route path="/Registro" component={Registro} />
+      <Route path="/modificar" component={Modificar} />
+    </Switch>
+    </Router>
 )
